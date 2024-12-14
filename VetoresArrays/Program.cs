272ae61d8c7e94b53d13027f0754484f1556390f
Evandro2270 <evandro535@gmail.com>
2024-12-14 16:37:45 -0300

    Adicionar arquivos de projeto.

---
+++ VetoresArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                string[] nomes = { "Maria", "Jose", "Ana" };
+                double[] valores = { 145.00 , 165.00, 175.00};
+
+                for (int i = 0; i < nomes.Length; i++) {
+                    Console.WriteLine(nomes[i]);
+                }
+                
+                Console.WriteLine();
+                for (int i = 0; i < valores.Length; i++)
+                {
+                    Console.WriteLine(valores[i]);
+                }
+
+            }
+        }
+    }
+}
